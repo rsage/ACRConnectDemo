01631aa93627828424b52a5757c06dea13d9eb7b
Sergei Rusinov <rsage@mail.ru>
2014-09-22 16:27:56 +0400

    Google Authentication.

---
+++ TestGoogle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGoogle.Startup))]
+namespace TestGoogle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
