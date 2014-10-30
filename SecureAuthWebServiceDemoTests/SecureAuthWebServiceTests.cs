86324cf9c9ac58de2a3562353235ecc74e596957
Sergei Rusinov <rsage@mail.ru>
2014-10-30 12:09:06 +0300

    User account creation using SecureAuth web-service.

--- SecureAuthWebServiceDemoTests/SecureAuthWebServiceTests.cs
+++ SecureAuthWebServiceDemoTests/SecureAuthWebServiceTests.cs
@@ -12,15 +12,14 @@ namespace SecureAuthWebServiceDemoTests
         private const string SvcLogin = "FBAService";
         private const string SvcPassword = "wh0Ar3Y06";
 
-        private const string UserLogin = "idtest1000";
-        private const string NewUserLogin = "idtest1010";
-
         [TestMethod]
         public void ReadProfileTest()
         {
+            const string userName = "idtest1050";
+
             var client = new ProfileClient();
 
-            var profile = client.ReadProfile(SvcLogin, SvcPassword, UserLogin);
+            var profile = client.ReadProfile(SvcLogin, SvcPassword, userName);
 
             Assert.IsNotNull(profile);
 
@@ -31,23 +30,71 @@ namespace SecureAuthWebServiceDemoTests
         }
 
         [TestMethod]
+        public void CreateUserAccountTest()
+        {
+            const string userName = "idtest1050";
+            const string password = "B0okStores#";
+            const string email = "srusinov@acr.org";
+            const string firstName = "John";
+            const string lastName = "Smith-" + userName;
+            const string phone = "111-111-1111";
+            const string institutionAddress = "Preston White Drive 101";
+            const string personalAddress = "North Pole";
+            const string institution = "ACR";
+            const string department = "QA";
+            const string jobTitle = "Head";
+
+            var membershipClient = new MembershipClient();
+
+            MembershipCreateStatus status;
+            var user = membershipClient.CreateUser(SvcLogin, SvcPassword, userName, password, email, null, null, true, null, out status);
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual(MembershipCreateStatus.Success, status);
+
+            var profileClient = new ProfileClient();
+
+            var properties = new[]
+            {
+                new UserProfileProperty { Key = "FirstName", Value = firstName}, 
+                new UserProfileProperty { Key = "LastName", Value = lastName}, 
+                new UserProfileProperty { Key = "Phone1", Value = phone}, 
+                new UserProfileProperty { Key = "Email1", Value = email}, 
+                new UserProfileProperty { Key = "AuxID5", Value = institutionAddress}, 
+                new UserProfileProperty { Key = "AuxID6", Value = personalAddress}, 
+                new UserProfileProperty { Key = "AuxID8", Value = institution}, 
+                new UserProfileProperty { Key = "AuxID9", Value = department}, 
+                new UserProfileProperty { Key = "AuxID10", Value = jobTitle},
+                new UserProfileProperty { Key = "AuxID1", Value = "FALSE"},
+            };
+
+            var saved = profileClient.SaveProfile(SvcLogin, SvcPassword, userName, properties);
+
+            Assert.IsTrue(saved);
+        }
+
+        [TestMethod]
         public void SaveProfileTest()
         {
+            const string userName = "idtest1050";
             var client = new ProfileClient();
 
             var properties = new[]
             {
-                new UserProfileProperty { Key = "FirstName", Value = "John"}, 
-                new UserProfileProperty { Key = "LastName", Value = "Smith"}, 
-                new UserProfileProperty { Key = "Phone1", Value = "111-111-1111"}, 
-                new UserProfileProperty { Key = "Email1", Value = "srusinov@acr.org"}, 
-                new UserProfileProperty { Key = "StreetAddress", Value = "Preston White Drive 101"}, 
-                new UserProfileProperty { Key = "AuxID8", Value = "ACR"}, 
-                new UserProfileProperty { Key = "AuxID9", Value = "QA"}, 
-                new UserProfileProperty { Key = "AuxID10", Value = "Head"} 
+                //new UserProfileProperty { Key = "FirstName", Value = "John"}, 
+                //new UserProfileProperty { Key = "LastName", Value = "Smith1020"}, 
+                //new UserProfileProperty { Key = "Phone1", Value = "111-111-1111"}, 
+                //new UserProfileProperty { Key = "Email1", Value = "srusinov@acr.org"}, 
+                //new UserProfileProperty { Key = "StreetAddress", Value = "Preston White Drive 101"}, 
+                //new UserProfileProperty { Key = "AuxID5", Value = "Preston White Drive"}, 
+                //new UserProfileProperty { Key = "AuxID6", Value = "North Pole"}, 
+                //new UserProfileProperty { Key = "AuxID8", Value = "ACR"}, 
+                //new UserProfileProperty { Key = "AuxID9", Value = "QA"}, 
+                //new UserProfileProperty { Key = "AuxID10", Value = "Head"},
+                new UserProfileProperty { Key = "AuxID1", Value = "FALSE"},
             };
 
-            var created = client.SaveProfile(SvcLogin, SvcPassword, UserLogin, properties);
+            var created = client.SaveProfile(SvcLogin, SvcPassword, userName, properties);
 
             Assert.IsTrue(created);
         }
@@ -55,14 +102,25 @@ namespace SecureAuthWebServiceDemoTests
         [TestMethod]
         public void CreateUserTest()
         {
+            const string userName = "idtest1050";
             var client = new MembershipClient();
 
             MembershipCreateStatus status;
-            var user = client.CreateUser(SvcLogin, SvcPassword, NewUserLogin, "Passw0rd!", "srusiniv@acr.org", "question", "answer", true, null, out status);
+            var user = client.CreateUser(SvcLogin, SvcPassword, userName, "B0okStores#", "srusiniv@acr.org", "question", "answer", true, null, out status);
 
             Assert.IsNotNull(user);
-            Assert.AreEqual(NewUserLogin, user.UserName);
             Assert.AreEqual(MembershipCreateStatus.Success, status);
+        }
+
+        [TestMethod]
+        public void ChangePasswordTest()
+        {
+            const string userName = "idtest1050";
+            var client = new MembershipClient();
+
+            var result = client.ChangePassword(SvcLogin, SvcPassword, userName, "Passw0rd!", "B0okStores#");
+
+            Assert.IsTrue(result);
         }
     }
 }
